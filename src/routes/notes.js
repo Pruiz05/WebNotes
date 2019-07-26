4a6fa2b4451948ebf5f423c05baa90c5da929d39
PedroLuis <pedroluis0542@gmail.com>
2019-07-25 23:13:18 -0400

    Agregando pagina de Crear nueva nota

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -1,6 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/notes/add', (req, res) => {
+    res.render('notes/new-note');
+});
+
+router.post('/notes/new-note', (req, res)=>{
+    //validacioones de formulario
+    const {title, description} = req.body;
+    const errors =[];
+    //valores enviados vacios
+    if (!title) {
+        errors.push({text: 'Please Write a Title'})
+    }
+    if (!description) {
+        errors.push({text: 'Please Write a Description'})
+    }
+
+    //
+    if(errors.length > 0){
+        res.render('notes/new-note', {
+            errors, 
+            title,
+            description
+        })
+    }else{
+        res.send('ok');
+    }
+    
+    //console.log(req.body); 
+    //res.send('ok');
+});
+
 router.get('/notes', (req, res)=>{
     res.send('Notas de la base de datos');
 });
